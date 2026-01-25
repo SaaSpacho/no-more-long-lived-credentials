7fb224232ee2bd971d9796cff353fb8b54667322
Rob Kenis <rob.kenis@hotmail.com>
2026-01-25 10:18:40 +0100

    Allow public access on function url

--- aws/deploy/lambda.tf
+++ aws/deploy/lambda.tf
@@ -79,6 +79,14 @@ resource "aws_lambda_function" "this" {
   architectures = ["x86_64"]
 }
 
+resource "aws_lambda_permission" "function_url" {
+  statement_id  = "AllowPublicAccess"
+  action        = "lambda:InvokeFunctionUrl"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "*"
+  function_url_auth_type = "NONE"
+}
+
 resource "aws_lambda_function_url" "this" {
   function_name      = aws_lambda_function.this.function_name
   authorization_type = "NONE"
